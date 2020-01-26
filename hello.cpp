9963bf9344b9be6d5e1f9aeede5362d3e3a7065b
Nidhi Agarwal <nidhi.agrl12@gmail.com>
2020-01-25 21:56:12 -0800

    cicd

--- hello.cpp
+++ hello.cpp
@@ -7,6 +7,6 @@ b=6;
 x = a+b;
 printf("this is first checkin of C code %d\n", x);
 printf("created new branch called branch-1\n");
-printf("this is first checkin of C code IN MASTER injected conflict %d", x);
+printf("this is first checkin of C code CI/CD conflict %d", x);
 }
 
