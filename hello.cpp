839993e4f6f710229170201340be418fc6a30115
nidhiagrl12 <54427342+nidhiagrl12@users.noreply.github.com>
2020-01-25 20:54:39 -0800

    modified cpp on branch-1 (#1)
    optional message

--- hello.cpp
+++ hello.cpp
@@ -5,6 +5,8 @@ int x, a,b;
 a=5;
 b=6;
 x = a+b;
+printf("this is first checkin of C code %d\n", x);
+printf("created new branch called branch-1\n");
 printf("this is first checkin of C code IN MASTER injected conflict %d", x);
 }
 
